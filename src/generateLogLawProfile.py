a8b6de2352614f509f003989535a6936a2b3c249
Ting-Hsuan (Dustin) Ma <tma@megalon.dhcp.lbl.gov>
2024-07-03 10:12:30 -0700

    applied same changes to analytical profile

--- src/generateLogLawProfile.py
+++ src/generateLogLawProfile.py
@@ -19,13 +19,19 @@ u = u_tau * (1./ kappa * np.log(heights * Re_tau) + B)
 
 # Create v values (set all v values to 0)
 v = np.zeros_like(heights)
+mixing_ratio = np.zeros_like(heights)
+potential_temp = np.full_like(heights, 300.0)
 
 # Prepare data to write to file
-data = np.column_stack((heights, u, v))
+data_sounding = np.column_stack((heights, potential_temp, mixing_ratio, u, v))
+data_sponge = np.column_stack((heights, u, v))
 
 # Save data to file
-filename = "input_ReTau2000_loglaw.txt"
-np.savetxt(filename, data, header="y u v", fmt="%.8e", delimiter='\t')
+filename = "input_ReTau2000Ana"
+filetype = ".txt"
 
-print(f"Data saved to {filename}.")
+np.savetxt(filename+"_sounding"+filetype, data_sounding, header="y theta mr u v", fmt="%.8e", delimiter='\t')
+print(f"Data saved to {filename}_sounding{filetype}.")
+np.savetxt(filename+"_sponge"+filetype, data_sponge, header="y u v", fmt="%.8e", delimiter='\t')
+print(f"Data saved to {filename}_sponge{filetype}.")
 
